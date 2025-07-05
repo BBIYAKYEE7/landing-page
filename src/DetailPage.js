34542b5824a4985f33e87cc01f8dfb2f3ffaf227
BBIYAKYEE7 <bbiyakyee7@gmail.com>
2025-07-05 15:51:25 +0900

    💻 Detail Page 업데이트

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import './DetailPage.css';
+
+const appScreens = [
+  '/app1.png',
+  '/app2.png',
+  '/app3.png',
+];
 
 function DetailPage({ num }) {
+  const scrollRef = useRef();
+
   return (
     <div className="detail-page">
-      <h1>상세페이지 {num}</h1>
-      <p>여기에 상세 설명과 이미지를 추가하세요.</p>
+      <h1>U-TEED 앱 소개</h1>
+      <p>우리 서비스의 실제 앱 화면을 확인해보세요!</p>
+      <div className="app-screens-container" ref={scrollRef}>
+        {appScreens.map((src, idx) => (
+          <img key={idx} src={src} alt={`앱 화면 ${idx+1}`} className="app-screen-img" />
+        ))}
+      </div>
     </div>
   );
 }
